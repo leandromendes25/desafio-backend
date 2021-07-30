486c451e557f20ab701be24f5e876f82be176226
leandromendes25 <leandromiendes@yahoo.com>
2021-07-30 10:54:09 -0300

    service done

--- src/main/java/com/fcamara/desafiobackend/controller/EntradaSaidaController.java
+++ src/main/java/com/fcamara/desafiobackend/controller/EntradaSaidaController.java
@@ -1,15 +1,45 @@
 package com.fcamara.desafiobackend.controller;
 
+import com.fcamara.desafiobackend.model.Estabelecimento;
 import com.fcamara.desafiobackend.model.Veiculo;
+import com.fcamara.desafiobackend.repository.EstabelecimentoRepository;
+import com.fcamara.desafiobackend.repository.VeiculoRepository;
+import com.fcamara.desafiobackend.service.ControleEntradaSaidaService;
+import com.fcamara.desafiobackend.util.JsonResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/controle")
 public class EntradaSaidaController {
+    @Autowired
+    private ControleEntradaSaidaService service;
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+    @Autowired
+    private EstabelecimentoRepository estabelecimentoRepository;
 
-//    @PutMapping("/entrada/{veiculoId}")
-//    public ResponseEntity<?> entrada(@PathVariable Long veiculoId){
-//
-//    }
+    @PutMapping("/entrada/{veiculoId}/{estabelecimentoId}")
+    public ResponseEntity<?> entrada(@PathVariable Long veiculoId, @PathVariable Long estabelecimentoId){
+        Optional<Estabelecimento> estabelecimentoDb = estabelecimentoRepository.findById(estabelecimentoId);
+        Optional<Veiculo> veiculoDb = veiculoRepository.findById(veiculoId);
+        if(!estabelecimentoDb.isPresent() || !veiculoDb.isPresent()){
+            return ResponseEntity.badRequest().body(JsonResponse.message("Veiculo ou Estabelecimento Incorreto"));
+        }
+        return service.controleEntrada(veiculoDb.get(), estabelecimentoDb.get());
+    }
+
+    @PutMapping("/saida/{veiculoId}/{estabelecimentoId}")
+    public ResponseEntity<?> saida(@PathVariable Long veiculoId, @PathVariable Long estabelecimentoId){
+        Optional<Estabelecimento> estabelecimentoDb = estabelecimentoRepository.findById(estabelecimentoId);
+        Optional<Veiculo> veiculoDb = veiculoRepository.findById(veiculoId);
+        if(!estabelecimentoDb.isPresent() || !veiculoDb.isPresent()){
+            return ResponseEntity.badRequest().body(JsonResponse.message("Veiculo ou Estabelecimento Incorreto"));
+        }
+        return service.controleSaida(veiculoDb.get(), estabelecimentoDb.get());
+}
 }
--- src/main/java/com/fcamara/desafiobackend/service/ControleEntradaSaidaService.java
+++ src/main/java/com/fcamara/desafiobackend/service/ControleEntradaSaidaService.java
@@ -8,17 +8,19 @@ import com.fcamara.desafiobackend.repository.VeiculoRepository;
 import com.fcamara.desafiobackend.util.JsonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
-
-public class controleEntradaSaidaService {
+@Service
+public class ControleEntradaSaidaService {
     @Autowired
     private VeiculoRepository veiculorepository;
     @Autowired
     private EstabelecimentoRepository estabelecimentoRepository;
+
     //Necessário controle -> serviço
     public ResponseEntity<?>controleEntrada(Veiculo veiculo, Estabelecimento estabelecimento){
         //estacionado
@@ -28,30 +30,29 @@ public class controleEntradaSaidaService {
         if (veiculo.getTipo() == TipoVeiculo.CARRO && estabelecimento.getVagasCarros() > estabelecimento.getVagasOcupadasCarros()){
         estabelecimento.setVagasOcupadasCarros(+1);
         veiculo.setEstacionado(true);
-        veiculo.setEntrada(LocalDateTime.now());
+        veiculo.setEntrada(LocalTime.now());
         veiculorepository.save(veiculo);
-        estabelecimentoRepository.save(estabelecimento);
+        return ResponseEntity.ok().body(JsonResponse.entradaMensagem("Veiculo estacionado com sucesso", veiculo));
         }
         if (veiculo.getTipo() == TipoVeiculo.MOTO && estabelecimento.getVagasMotos() > estabelecimento.getVagasOcupadasMotos()) {
             estabelecimento.setVagasOcupadasMotos(+1);
             veiculo.setEstacionado(true);
-            veiculo.setEntrada(LocalDateTime.now());
+            veiculo.setEntrada(LocalTime.now());
             veiculorepository.save(veiculo);//necessário salvar -> banco
             estabelecimentoRepository.save(estabelecimento);
-        }
-        if (estabelecimento.getVagasMotos().equals(estabelecimento.getVagasOcupadasMotos()) || estabelecimento.getVagasOcupadasCarros().equals(estabelecimento.getVagasCarros())){
-            return ResponseEntity.badRequest().body(JsonResponse.message("Vagas esgotadas"));
+            return ResponseEntity.ok().body(JsonResponse.entradaMensagem("Veiculo estacionado com sucesso", veiculo));
         }
         else return ResponseEntity.badRequest().body(JsonResponse.message("Erro ao dar entrada"));
+
     }
+
     public ResponseEntity<?> controleSaida(Veiculo veiculo, Estabelecimento estabelecimento){
         if (veiculo.isEstacionado() ){
-            LocalDateTime saida;
             if(veiculo.getTipo().equals(TipoVeiculo.CARRO)){
                 estabelecimento.setVagasOcupadasCarros(-1);
             }else estabelecimento.setVagasMotos(-1);
             veiculo.setEstacionado(false);
-
+            veiculorepository.save(veiculo);
             long tempoEstacionado = veiculo.getEntrada().until(LocalTime.now(), ChronoUnit.HOURS);
             double valor = estabelecimento.getValorHora();
             if(tempoEstacionado > 0) {
